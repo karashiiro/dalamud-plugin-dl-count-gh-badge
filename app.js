950ee7e9b91f321b63019773b497af31ac37049c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-08-24 16:31:41 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const bent = require("bent");
+const express = require("express");
+
+const app = express();
+
+const getDownloadCounts = bent("https://raw.githubusercontent.com/goatcorp/DalamudPlugins/master/downloadcounts.json", "json");
+
+// Routes
+app.get('/:PluginName', async (req, res) => {
+  const downloadCounts = await getDownloadCounts();
+
+  let count = 0;
+  if (downloadCounts[req.params.PluginName]) {
+    count = downloadCounts[req.params.PluginName].count;
+  }
+  
+  const format = {
+    schemaVersion: 1,
+    label: "Downloads",
+    message: `${count}`,
+    color: "green",
+  };
+
+  
+  res.send(format);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Internal Serverless Error');
+});
+
+module.exports = app;
